c8d9b5179b341b61d456204a74549c7bf5f27804
Shehab44 <133755906+Shehab44@users.noreply.github.com>
2026-08-23 09:04:34 +0100

    fix: item 3 - move and document orphan scripts

--- check-names.js
+++
@@ -1,7 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-async function m() { 
-  const prods = await p.product.findMany({ take: 5, select: { matCode: true, nameAr: true } }); 
-  console.log(prods); 
-} 
-m().finally(() => p.$disconnect());
